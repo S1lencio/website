f584e0c54707fa3538c873d72ea3424feedae964
Silencio <77356433+S1lencio@users.noreply.github.com>
2024-11-26 21:38:07 +0100

    blog post? and some more shit

--- src/app/contact/tiles.tsx
+++ src/app/contact/tiles.tsx
@@ -1,20 +1,20 @@
 const contactLinks = [
     {
         platform: "Discord",
-        url: "https://discord.com/users/438730703417376769", // Replace with your Discord link
-        icon: " ", // You can use an icon from any icon library
+        url: "https://discord.com/users/438730703417376769",
+        icon: " ",
         name: "@fuck_you_discrd"
     },
     {
         platform: "Matrix",
-        url: "https://matrix.to/#/@silencio:devlencio.net", // Replace with your Matrix link
-        icon: "[m]", // Custom icon for Matrix
+        url: "https://matrix.to/#/@silencio:devlencio.net",
+        icon: "[m]",
         name: "@silencio:devlencio.net"
     },
     {
         platform: "GitHub",
-        url: "https://github.com/S1lencio", // Replace with your GitHub profile link
-        icon: "󰊤 ", // GitHub icon
+        url: "https://github.com/S1lencio",
+        icon: "󰊤 ",
         name: "S1lencio"
     },
 ];
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,19 +15,21 @@ export default function Page() {
           </h2>
 
           <h2 className="mt-8">
-              You might want to look through my <Link  href="/projects" className="text-indigo-500 hover:text-indigo-600 transition-colors">projects</Link>.
+              You might want to look through my <Link href="/projects"
+                                                      className="text-indigo-500 hover:text-indigo-600 transition-colors">projects</Link>.
           </h2>
           <h2>
-              If you want to contact me, check out the <Link  href="/contact" className="text-indigo-500 hover:text-indigo-600 transition-colors">contact</Link> page.
+              If you want to contact me, check out the <Link href="/contact"
+                                                             className="text-indigo-500 hover:text-indigo-600 transition-colors">contact</Link> page.
           </h2>
           <h2>
-              If you want to know more about me, check out the <Link  href="/about" className="text-indigo-500 hover:text-indigo-600 transition-colors">about</Link> page.
+              I have a <Link href="/blog"
+                             className="text-indigo-500 hover:text-indigo-600 transition-colors">blog</Link>, because of
+              course I do.
           </h2>
-          <h2>
-              Also check out my <Link  href="/links" className="text-indigo-500 hover:text-indigo-600 transition-colors">links</Link>!
-          </h2>
-          <h2>
-              If you want to see my <Link  href="/blog" className="text-indigo-500 hover:text-indigo-600 transition-colors">blog</Link>, you can do so here.
+
+          <h2 className="mt-8">
+              This page will grow and evolve alongside myself over time, and I have a lot of time to spend on useless and irrelevant projects.
           </h2>
       </div>
   );
